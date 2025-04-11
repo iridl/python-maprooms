ac88734cc77022b02698657b3156f6f3421d74d6
remic <remic@iri.columbia.edu>
2025-04-11 08:15:03 -0400

    synthetic geometries

--- enacts/onset/maproom.py
+++ enacts/onset/maproom.py
@@ -29,8 +29,6 @@ PRECIP_PARAMS = {
     "variable": "precip", "time_res": "daily", "ds_conf": GLOBAL_CONFIG["datasets"]
 }
 
-ADMIN_CONFIG = GLOBAL_CONFIG["datasets"]["shapes_adm"]
-
 def register(FLASK, config):
 
     PFX = f'{GLOBAL_CONFIG["url_path_prefix"]}/{config["core_path"]}'
@@ -249,13 +247,15 @@ def register(FLASK, config):
         ] + [
             mapr_u.make_adm_overlay(
                 adm_name=adm["name"],
-                adm_geojson=calc.sql2GeoJSON(adm["sql"], GLOBAL_CONFIG["db"]),
-                adm_clor=adm["color"],
+                adm_geojson=calc.geom2GeoJSON(
+                    calc.get_geom(level=i, conf=GLOBAL_CONFIG)
+                ),
+                adm_color=adm["color"],
                 adm_lev=i+1,
-                adm_weight=len(ADMIN_CONFIG)-i,
+                adm_weight=len(GLOBAL_CONFIG["datasets"]["shapes_adm"])-i,
                 is_checked=adm["is_checked"],
             )
-            for i, adm in enumerate(ADMIN_CONFIG)
+            for i, adm in enumerate(GLOBAL_CONFIG["datasets"]["shapes_adm"])
         ] + [
             dlf.Overlay(
                 dlf.TileLayer(
@@ -1074,9 +1074,7 @@ def register(FLASK, config):
         map_data = map_data.rename(X="lon", Y="lat")
         map_data.attrs["scale_min"] = map_min
         map_data.attrs["scale_max"] = map_max
-        clip_shape = calc.sql2geom(
-            ADMIN_CONFIG[0]['sql'], GLOBAL_CONFIG["db"]
-        )["the_geom"][0]
+        clip_shape = calc.get_geom(level=0, conf=GLOBAL_CONFIG)["the_geom"][0]
         result = pingrid.tile(map_data, tx, ty, tz, clip_shape)
         return result
 
