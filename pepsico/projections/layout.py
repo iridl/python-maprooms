c16292a01a43bd0e8471a9edbaad0bca35715368
Rémi Cousin <42619415+remicousin@users.noreply.github.com>
2025-11-06 11:16:49 -0500

    factoring out common layout functionality (#555)
    * factoring out common layout functionality
    
    * removing _layout suffix and unused controversial function

--- pepsico/projections/layout.py
+++ pepsico/projections/layout.py
@@ -1,8 +1,7 @@
-from dash import dcc
 from dash import html
-import dash_bootstrap_components as dbc
-import dash_leaflet as dlf
 from fieldsets import Block, Select, PickPoint, Month, Number
+import layout_utilities as lou
+
 
 from globals_ import GLOBAL_CONFIG
 
@@ -13,85 +12,9 @@ LIGHT_GRAY = "#eeeeee"
 
 def app_layout():
 
-    return dbc.Container(
-        [
-            dcc.Location(id="location", refresh=True),
-            navbar_layout(),
-            dbc.Row(
-                [
-                    dbc.Col(
-                        controls_layout(),
-                        sm=12, md=4,
-                        style={
-                            "background-color": "white", "border-style": "solid",
-                            "border-color": LIGHT_GRAY,
-                            "border-width": "0px 1px 0px 0px",
-                        },
-                    ),
-                    dbc.Col(
-                        [
-                            dbc.Row(
-                                [
-                                    dbc.Col(
-                                        map_layout(),
-                                        width=12,
-                                        style={"background-color": "white"},
-                                    ),
-                                ],
-                            ),
-                            dbc.Row(
-                                [
-                                    dbc.Col(
-                                        results_layout(),
-                                        width=12,
-                                        style={
-                                            "background-color": "white",
-                                            "min-height": "100px",
-                                            "border-style": "solid",
-                                            "border-color": LIGHT_GRAY,
-                                            "border-width": "1px 0px 0px 0px",
-                                        },
-                                    ),
-                                ],
-                            ),
-                        ],
-                        sm=12,
-                        md=8,
-                        style={"background-color": "white"},
-                    ),
-                ],
-            ),
-        ],
-        fluid=True,
-        style={"padding-left": "0px", "padding-right": "0px"},
-    )
+    return lou.app_1(
 
-
-def help_layout(buttonname, id_name, message):
-    return html.Div(
-        [
-            html.Label(
-                f"{buttonname}:", id=id_name,
-                style={"cursor": "pointer","font-size": "100%","padding-left":"3px"},
-            ),
-            dbc.Tooltip(f"{message}", target=id_name, className="tooltiptext"),
-        ]
-    )
-
-
-def navbar_layout():
-    return dbc.Nav(
-        [
-            html.A(
-                dbc.Row(
-                    [
-                        dbc.Col(
-                            dbc.NavbarBrand("CCA", className="ml-2")
-                        ),
-                    ],
-                    align="center", style={"padding-left": "5px", "color": "white"},
-                ),
-            ),
+        lou.navbar("CCA",  
             Block("Region",
                 Select(
                     id="region",
@@ -191,30 +114,14 @@ def navbar_layout():
                 ),
                 button_id="submit_controls",
             ),
-            dbc.Alert(
-                "Something went wrong",
-                color="danger",
-                dismissable=True,
-                is_open=False,
-                id="map_warning",
-                style={"margin-bottom": "8px"},
-            ),
-        ],
-        style={"background-color": IRI_GRAY},
-    )
+        ),
 
-
-def controls_layout():
-    return dbc.Container(
-        [
-            html.H5(["Climate Change Analysis"]),
-            html.P(
-                """
-                This Maproom displays seasonal projected change of key climate
-                variables with respect to historical records.
-                """
-            ),
-            dcc.Loading(html.P(id="map_description"), type="dot"),
+        lou.description(
+            "Climate Change Analysis",
+            """
+            This Maproom displays seasonal projected change of key climate
+            variables with respect to historical records.
+            """,
             html.P(
                 """
                 Use the controls in the top banner to choose other variables, models,
@@ -236,62 +143,11 @@ def controls_layout():
                 which it is the relative difference (in %).
                 """
             ),
-        ],
-        fluid=True, className="scrollable-panel",
-        style={"padding-bottom": "1rem", "padding-top": "1rem"},
+        ),
+        
+        lou.map(GLOBAL_CONFIG["zoom"]),
+        
+        lou.local_single_tabbed(
+            "Local History and Projections", download_button=True
+        ),
     )
-
-
-def map_layout():
-    return dbc.Container(
-        [
-            dcc.Loading(html.H5(
-                id="map_title",
-                style={
-                    "text-align":"center", "border-width":"1px",
-                    "border-style":"solid", "border-color":"grey",
-                    "margin-top":"3px", "margin-bottom":"3px",
-                },
-            ),  type="dot"),
-            dcc.Loading(dlf.Map(
-                [
-                    dlf.LayersControl(id="layers_control", position="topleft"),
-                    dlf.LayerGroup(
-                        [dlf.Marker(id="loc_marker", position=(0, 0))],
-                        id="layers_group"
-                    ),
-                    dlf.ScaleControl(imperial=False, position="bottomright"),
-                    dlf.Colorbar(
-                        id="colorbar",
-                        nTicks=9,
-                        opacity=1,
-                        tooltip=True,
-                        position="topright",
-                        width=10,
-                        height=300,
-                        className="p-1",
-                        style={
-                            "background": "white", "border-style": "inset",
-                            "-moz-border-radius": "4px", "border-radius": "4px",
-                            "border-color": "LightGrey",
-                        },
-                    ),
-                ],
-                id="map",
-                center=None,
-                zoom=GLOBAL_CONFIG["zoom"],
-                style={"width": "100%", "height": "50vh"},
-            ), type="dot"),
-        ],
-        fluid=True,
-    )
-
-
-def results_layout():
-    return dbc.Tabs([dbc.Tab([
-        html.Div([
-            dbc.Button("Download data in graph", id="btn_csv", size="sm"),
-            dcc.Download(id="download-dataframe-csv"),
-        ], className="d-grid justify-content-md-end"),
-        html.Div([dbc.Spinner(dcc.Graph(id="local_graph"))]),
-    ], label="Local History and Projections")])
