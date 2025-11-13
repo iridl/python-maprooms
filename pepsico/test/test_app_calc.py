b16bc3295a92b66a3af2274cf4d3f6bc18f4c49a
Rémi Cousin <remic@iri.columbia.edu>
2025-11-13 11:54:12 -0500

    alternative to _cumsum_flagged_diff

--- pepsico/test/test_app_calc.py
+++ pepsico/test/test_app_calc.py
@@ -57,6 +57,22 @@ def test__cumsum_flagged_diff_coord_size_1():
     assert spells1 == 1
     assert spells0 == 0
 
+
+def test_spells_length():
+
+    t = pd.date_range(start="2000-05-01", end="2000-05-10", freq="1D")
+    values = np.array([1, 0, 1, 1, 0, 0, 1, 1, 1, 0])
+    precip = xr.DataArray(values, coords={"T": t})
+    spells = app_calc.spells_length(precip, "T")
+    expected = [1, np.nan, np.nan, 2, np.nan, np.nan, np.nan, np.nan, 3, np.nan]
+
+    np.testing.assert_array_equal(spells.isnull(), np.isnan(expected))
+    np.testing.assert_array_equal(
+        spells.where(~spells.isnull(), other=0),
+        np.where(np.isnan(expected), 0, expected)
+    )
+
+
 def test_count_days_in_spells():
 
     t = pd.date_range(start="2000-05-01", end="2000-05-10", freq="1D")
@@ -92,4 +108,3 @@ def test_median_length_of_spells():
     mendian_spell = app_calc.median_length_of_spells(precip, "T")
 
     assert mendian_spell == 2
-    
